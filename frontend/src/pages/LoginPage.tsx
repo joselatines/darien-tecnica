51e760b6d9032ba198e006ad29129b9d75ffaa1f
Jose Latines <joselatinestranslator@gmail.com>
2025-09-24 19:40:16 -0400

    feat: configure boostrap

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center">
+      <input type="text" />
+      <button>Entrar</button>
+    </div>
+  )
+}
