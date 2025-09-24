401058569e38b379e32a607e1e0a6798b693ffff
Jose Latines <joselatinestranslator@gmail.com>
2025-09-24 19:25:20 -0400

    feat: configure react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import EspaciosPage from './pages/EspaciosPage.tsx'
+import MainLayout from './layouts/MainLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="dashboard" element={<MainLayout />}>
+          <Route path="espacios" element={<EspaciosPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
